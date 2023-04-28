241488b452cbe0f502e97742b03ff392180febda
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-04-28 03:05:23 -0500

    added content-card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {ContentCardComponent} from './content-card/content-card.component';
+import { ContentCardsComponent } from './content-cards/content-cards.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentCardComponent,
+    ContentCardsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/content-cards/content-cards.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-cards',
+  templateUrl: './content-cards.component.html',
+  styleUrls: ['./content-cards.component.scss']
+})
+export class ContentCardsComponent {
+
+}
